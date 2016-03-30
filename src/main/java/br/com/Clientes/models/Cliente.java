9439496c04978f0f95a7030c9335cff073f7fe00
Guilherme <Guilherme@Guilherme-PC>
2016-03-30 15:08:56 -0300

    .

---
+++ src/main/java/br/com/Clientes/models/Cliente.java
@@ -0,0 +1,20 @@
+package br.com.Clientes.models;
+
+
+public class Cliente {
+
+	public Cliente() {
+		
+		private Long id;
+		private String nome;
+		private Long cpf;
+		private Date dataNascimento;
+		private String sexo;
+		private String senha;
+		private Boolean receberOfertaPorEmail;
+		
+		
+		
+	}
+
+}
